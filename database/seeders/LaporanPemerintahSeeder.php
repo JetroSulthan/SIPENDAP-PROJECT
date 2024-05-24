bf9e82ec487d25c4b9a8cfa9bdd12a522e7c61b6
JetroSulthan <jetrosulthan9626@gmail.com>
2024-05-24 07:13:26 +0700

    update Seeder

--- database/seeders/LaporanPemerintahSeeder.php
+++ database/seeders/LaporanPemerintahSeeder.php
@@ -2,19 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\Kios;
-use Illuminate\Database\Seeder;
+use App\Models\LaporanPemerintah;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
 
-class KiosSeeder extends Seeder
+class LaporanPemerintahSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Kios::create([
-            'laporan' => 'Tester File.pdf'
+        LaporanPemerintah::create([
+            'laporan' => 'Testing(bkn file asli).pdf'
         ]);
     }
 }
